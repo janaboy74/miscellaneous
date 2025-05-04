4c73b85a6bf98d9ea7bed21d113ebd47b41ff9ff
Klingl János <janaboy@gmail.com>
2025-05-04 18:06:09 +0200

    Added CMakeLists.txt to chkspd project. Comments converted to unix charset. Rework of caclulation.

--- checkspd/chkspd.cpp
+++ checkspd/chkspd.cpp
@@ -1,217 +1,173 @@
-#include <math.h>
-#include <time.h>
-#include <stdio.h>
+#include <cstdio>
 #include <chrono>
-#include <thread>
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-#include <stdint.h>
-#ifndef __linux__
-#include <windows.h>
-#endif // __linux__
-#ifdef __GNUC__
-#include <unistd.h>
-#else
-const int CLOCK_REALTIME = 0;
-int clock_gettime(int, struct timespec *spec) {
-    int64_t wintime;
-    GetSystemTimeAsFileTime((FILETIME*)&wintime);
-    wintime      -=116444736000000000LL;
-    spec->tv_sec  =wintime / 10000000LL;
-    spec->tv_nsec =wintime % 10000000LL *100;
-    return 0;
-};
-#endif
+#include <cstdint>
 
-double time(timespec &start_time) {
-		clock_gettime(CLOCK_REALTIME,&start_time);
-		return start_time.tv_sec+start_time.tv_nsec*1e-9;
+// optimaliz√°l√°s n√©lk√ºl ford√≠tva m≈±k√∂dik csak √©s stdc++11 kell hozz√°
+
+double clk() {
+    return std::chrono::duration_cast< std::chrono::nanoseconds >( std::chrono::system_clock::now().time_since_epoch() ).count() * 1e-9;
 }
 
-timespec times;
-/* A kˆvetkezı sˆrˆk akkor kellenek ha a C perdÌtı nem eszi meg a kÛdot */
-/* #define CLOCKS_PER_SEC 50 */
-#define HZ (CLOCKS_PER_SEC)
-
-long loop;
+uint64_t loop;
 double dif;
 double mul;
 
-float calcmips(void)
-{
- long rate;
- long i;
- register long c;
- double mips,rt;
- double t1,t2;
- mips=0; /* ez egy jÛ referencia */
- c=0;
- t1=0;
- t2=0;
- mul=5;
- rate=1;
- rt=1.0;
- while (mul>.01)
- {
- dif=0;
-  while (dif<0.1)
-  {
-   rt*=(1.0+mul);
-   rate=(long)rt;
-   t1=time(times);
-   for(i=0;i<rate;i++)
-   {
-     c+=0; /* NÈgy alapm˚velet lemÈrÈse */
-     c-=0;
-     c*=1;
-     c/=1;
-   }
-   t2=time(times);
-   dif=t2-t1;
-  }
-  rt/=(1.0+mul);
-  mul/=2.0; /* idıkalibr·l·s */
- }
- rate*=100; /* kb 1-2 sec */
- t1=time(times);
- for(i=0;i<rate;i++)
- {
-  c+=0; /* Mips meghat·roz·sa */
-  c-=0;
-  c*=1;
-  c/=1;
- }
- t2=time(times);
- dif=t2-t1; /*  mennyi idıegysÈg ? */
- if (dif!=0.0) mips=(5.0*rate)/(dif*1000000.0);
-             /* mennyi mips ? */
- loop=rate;
- return(mips);
+float calcmips() {
+    uint64_t rate;
+    uint64_t i;
+    uint64_t c;
+    double mips, rt;
+    double t1, t2;
+    mips = 0; /* ez egy j√≥ referencia */
+    c = 1;
+    mul = 5;
+    rate = 1;
+    rt = 1.0;
+    while( mul >.01 ) {
+        dif=0;
+        while( dif< 0.1 ) {
+            rt *= 1.0 + mul;
+            rate = ( uint64_t ) rt;
+            t1 = clk();
+            for( i=0; i < rate; ++i ) {
+                c += 0; /* N√©gy alapm√ªvelet lem√©r√©se */
+                c -= 0;
+                c *= 1;
+                c /= 1;
+            }
+            t2 = clk();
+            dif = t2 - t1;
+        }
+        rt /= 1.0 + mul;
+        mul /= 2.0; /* id√µkalibr√°l√°s */
+    }
+    rate *= 20; /* kb 1-2 sec */
+    t1 = clk();
+    for( i = 0; i < rate; ++i ) {
+        c += 0; /* Mips meghat√°roz√°sa */
+        c -= 0;
+        c *= 1;
+        c /= 1;
+    }
+    t2 = clk();
+    dif = t2 - t1; /*  mennyi id√µegys√©g ? */
+    if( dif!=0.0 )
+        mips = ( 5.0 * rate ) / ( dif * 1000000.0 );
+    /* mennyi mips ? */
+    loop = rate;
+    return mips;
 }
 
 
-float calcmflops(void)
-{
- long i,rate;
- register float c;
- double mflops,rt;
- double t1,t2;
- mflops=0;
- c=0;
- t1=0;
- t2=0;
- dif=0;
+float calcmflops() {
+    uint64_t i, rate;
+    float c;
+    double mflops, rt;
+    double t1, t2;
+    mflops = 0;
+    c = 1;
+    dif = 0;
 
- mul=5;
- rate=1;
- rt=1.0;
- while (mul>.1)
- {
- dif=0;
-  while (dif<0.1)
-  {
-   rt*=(1.0+mul);
-   rate=(long)rt;
-   t1=time(times);
-   for(i=0;i<rate;i++)
-   {
-     c+=0.0; /* Ugyn˙gy mint CPU-n·l */
-     c-=0.0; /* NÈgy alapm˚velet lemÈrÈse */
-     c*=1.0;
-     c/=1.0;
-   }
-   t2=time(times);
-   dif=t2-t1;
-  }
-  rt/=(1.0+mul);
-  mul/=2.0; /* idıkalibr·l·s */
- }
- rate*=100; /*  kb 1-2 sec */
- t1=time(times);
- for(i=0;i<rate;i++)
- {
-  c+=0.0; /* Flops meghat·roz·sa */
-  c-=0.0;
-  c*=1.0;
-  c/=1.0;
- }
- t2=time(times);
- dif=t2-t1; /*  mennyi idıegysÈg ? */
- if (dif!=0.0) mflops=(4.0*rate)/(dif*1000000.0);
-          /* mennyi flops ? */
- loop=rate;
- return(mflops);
+    mul = 5;
+    rate = 1;
+    rt = 1.0;
+    while( mul >.1 ) {
+        dif = 0;
+        while( dif < 0.1 ) {
+            rt *= 1.0 + mul;
+            rate = ( uint64_t ) rt;
+            t1 = clk();
+            for( i = 0; i < rate; ++i ) {
+                c += 0.0; /* Ugyn√∫gy mint CPU-n√°l */
+                c -= 0.0; /* N√©gy alapm√ªvelet lem√©r√©se */
+                c *= 1.0;
+                c /= 1.0;
+            }
+            t2 = clk();
+            dif = t2 - t1;
+        }
+        rt /= 1.0 + mul;
+        mul /= 2.0; /* id√µkalibr√°l√°s */
+    }
+    rate *= 20; /*  kb 1-2 sec */
+    t1 = clk();
+    for( i = 0; i < rate; ++i ) {
+        c += 0.0; /* Flops meghat√°roz√°sa */
+        c -= 0.0;
+        c *= 1.0;
+        c /= 1.0;
+    }
+    t2 = clk();
+    dif = t2 - t1; /*  mennyi id√µegys√©g ? */
+    if( dif != 0.0 )
+        mflops = ( 4.0 * rate ) / ( dif * 1000000.0 );
+    /* mennyi flops ? */
+    loop = rate;
+    return mflops;
 }
 
-int main(int argc, char **argv)
-{
- float mips,mipsdif,mflops,mflopsdif;
- long mipsloop,mflopsloop;
- float rel1,rel2;
+int main( int argc, char **argv ) {
+    float mips, mipsdif, mflops, mflopsdif;
+    long mipsloop, mflopsloop;
+    float rel1, rel2;
 
- mips=calcmips();
- mipsloop=loop;
- mipsdif=dif;
+    mips = calcmips();
+    mipsloop = loop;
+    mipsdif = dif;
 
- printf("%f mips (%ld loop,%f time)\n",mips,mipsloop,mipsdif);
+    printf( "%f mips ( %ld loop,%f second )\n", mips, mipsloop, mipsdif );
 
- mflops=calcmflops();
- mflopsloop=loop;
- mflopsdif=dif;
+    mflops = calcmflops();
+    mflopsloop = loop;
+    mflopsdif = dif;
 
- printf("%f mflops (%ld loop,%f time)\n",mflops,mflopsloop,mflopsdif);
+    printf( "%f mflops ( %ld loop,%f second )\n", mflops, mflopsloop, mflopsdif );
 
- rel1=(mips/2.3);
- rel2=(mflops/1.56);
+    rel1 = mips / 2.3;
+    rel2 = mflops / 1.56;
 
- printf("Relative: 040/25Mhz Amiga OS     |SAS/C |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: 040/25Mhz Amiga OS     |SAS/C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/35.2);
- rel2=(mflops/3.97);
+    rel1 = mips / 35.2;
+    rel2 = mflops / 3.97;
 
- printf("Relative: 060/50Mhz AmigaOS MDEV4|GNU-C |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: 060/50Mhz AmigaOS MDEV4|GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/82.5);
- rel2=(mflops/15.7);
+    rel1 = mips / 82.5;
+    rel2 = mflops / 15.7;
 
- printf("Relative: 060/50Mhz AmigaOS      |GNU-C |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: 060/50Mhz AmigaOS      |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/71.02);
- rel2=(mflops/18.21);
+    rel1 = mips / 71.02;
+    rel2 = mflops / 18.21;
 
- printf("Relative: AMDK5/100Mhz Linux OS  |GNU-C |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: AMDK5/100Mhz Linux OS  |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/205.8);
- rel2=(mflops/12.9);
+    rel1 = mips / 205.8;
+    rel2 = mflops / 12.9;
 
- printf("Relative: 603e/166Mhz Amiga OS   |S-PPC |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: 603e/166Mhz Amiga OS   |S-PPC |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/409.0);
- rel2=(mflops/52.8);
+    rel1 = mips / 409.0;
+    rel2 = mflops / 52.8;
 
- printf("Relative: AMDK6-2/500Mhz Linux OS|GNU-C |   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: AMDK6-2/500Mhz Linux OS|GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/1072.3);
- rel2=(mflops/931.9);
+    rel1 = mips / 2686.5;
+    rel2 = mflops / 1373.2;
 
- printf("Relative: ATHLON64 3000+ Windows |GCC422|   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: Ryzen R5 2400G Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/2748.8);
- rel2=(mflops/2154.5);
+    rel1 = mips / 2534.35;
+    rel2 = mflops / 1278.93;
 
- printf("Relative: Ryzen R5 2400G Win/Lin |GCC8.1|   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: Ryzen R5 2500U Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/2561.6);
- rel2=(mflops/2030.7);
+    rel1 = mips / 2906.85;
+    rel2 = mflops / 1480.19;
 
- printf("Relative: Ryzen R5 2500U Win/Lin |GCC8.2|   %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
+    printf( "Relative: Ryzen R7 2700  Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
- rel1=(mips/2879.9);
- rel2=(mflops/2304.6);
-
- printf("Relative: Ryzen R7 2700      Lin |GCC11.4|  %7.2f [ir]   %7.2f [fr]\n",rel1,rel2);
-
- return 0;
+    return 0;
 }
 
