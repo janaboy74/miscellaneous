9675dc04e762bc5ca151832372e786c25e36ef23
Klingl János <janaboy@gmail.com>
2025-05-04 18:55:14 +0200

    Added some Intel CPU-s ( MiniPC ).

--- checkspd/chkspd.cpp
+++ checkspd/chkspd.cpp
@@ -126,48 +126,57 @@ int main( int argc, char **argv ) {
     rel1 = mips / 2.3;
     rel2 = mflops / 1.56;
 
-    printf( "Relative: 040/25Mhz Amiga OS     |SAS/C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: 040/25Mhz       Amiga OS |SAS/C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 35.2;
     rel2 = mflops / 3.97;
 
-    printf( "Relative: 060/50Mhz AmigaOS MDEV4|GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: 060/50Mhz Amiga OS MDEV4 |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 82.5;
     rel2 = mflops / 15.7;
 
-    printf( "Relative: 060/50Mhz AmigaOS      |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: 060/50Mhz       Amiga OS |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 71.02;
     rel2 = mflops / 18.21;
 
-    printf( "Relative: AMDK5/100Mhz Linux OS  |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: AMDK5/100Mhz    Linux OS |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 205.8;
     rel2 = mflops / 12.9;
 
-    printf( "Relative: 603e/166Mhz Amiga OS   |S-PPC |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: 603e/166Mhz     Amiga OS |S-PPC |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 409.0;
     rel2 = mflops / 52.8;
 
-    printf( "Relative: AMDK6-2/500Mhz Linux OS|GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: AMDK6-2/500Mhz  Linux OS |GNU-C |   %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+
+    rel1 = mips / 2056.9;
+    rel2 = mflops / 1151.9;
+
+    printf( "Relative: Intel N5105     Linux OS |GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+
+    rel1 = mips / 2352.2;
+    rel2 = mflops / 1018.6;
+
+    printf( "Relative: Intel N4000     Linux OS |GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 2686.5;
     rel2 = mflops / 1373.2;
 
-    printf( "Relative: Ryzen R5 2400G Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: Ryzen R5 2400G  Linux OS |GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 2534.35;
     rel2 = mflops / 1278.93;
 
-    printf( "Relative: Ryzen R5 2500U Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: Ryzen R5 2500U  Linux OS |GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     rel1 = mips / 2906.85;
     rel2 = mflops / 1480.19;
 
-    printf( "Relative: Ryzen R7 2700  Linux OS|GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
+    printf( "Relative: Ryzen R7 2700   Linux OS |GCC13.3|  %7.2f [ir]   %7.2f [fr]\n", rel1, rel2 );
 
     return 0;
 }
-
